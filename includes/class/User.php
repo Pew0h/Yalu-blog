200ca30564ceb96c9680b84fcc7b91c6c7cc9e1c
Pewoh <burl410@gmail.com>
2021-05-04 17:38:38 +0200

    Update User.php

---
+++ includes/class/User.php
@@ -0,0 +1,62 @@
+<?php
+require('Database.php');
+class User
+{
+    public static function insertUser(string $prenom, string $nom, string $pseudo, string $email, string $password, int $role)
+    {
+        $database = Database::getInstance();
+        $request = $database->prepare('INSERT INTO utilisateur(prenom, nom, `pseudo`, `mot_de_passe`, email, id_role, date_inscription) VALUES (:user_prenom, :user_nom, :user_pseudo, :user_password, :user_email, :user_role, :user_inscription)');
+        $hashPassword = hash('sha512', $password);
+        echo $hashPassword;
+        $request->execute(array(
+            'user_prenom' => $prenom,
+            'user_nom' => $nom,
+            'user_pseudo' => $pseudo,
+            'user_password' => $hashPassword,
+            'user_email' => $email,
+            'user_inscription' => date('Y-m-d'),
+            'user_role' => $role
+        ));
+    }
+
+    public static function isUserExist(string $pseudo, string $password) : bool
+    {
+        $database = Database::getInstance();
+        $hashPassword = hash('sha512', $password);
+        $request = $database->prepare('SELECT `pseudo`, `mot_de_passe` FROM utilisateur WHERE `pseudo`= :user_pseudo AND `mot_de_passe` = :user_password');
+        $request->execute(array(
+            'user_pseudo' => $pseudo,
+            'user_password' => $hashPassword
+        ));
+
+        return $request->rowCount() >= 1;
+    }
+    
+    public static function getUserId(string $pseudo, string $password) : int
+    {
+        $database = Database::getInstance();
+        $hashPassword = hash('sha512', $password);
+        $request = $database->prepare('SELECT id_utilisateur FROM utilisateur WHERE `pseudo`= :user_pseudo AND `mot_de_passe` = :user_password');
+        $request->execute(array(
+            'user_pseudo' => $pseudo,
+            'user_password' => $hashPassword
+        ));
+        while($data = $request->fetch()){
+            return $data['id_utilisateur'];
+        }
+    }
+
+    public static function isInformationExist(string $pseudo, string $email) : bool
+    {
+        $database = Database::getInstance();
+        $request = $database->prepare('SELECT `pseudo`, email FROM utilisateur WHERE `pseudo`= :user_pseudo OR email = :user_email');
+        $request->execute(array(
+            'user_pseudo' => $pseudo,
+            'user_email' => $email
+        ));
+
+        return $request->rowCount() >= 1;
+    }
+}
+
+?>
