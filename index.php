d22ea8e612c1b72b54fff227fea843f5be26a2d9
yannbataillard <48249417+yannbataillard@users.noreply.github.com>
2021-05-04 19:53:30 +0200

    Layouts
    Ajout du footer et du header

--- index.php
+++ index.php
@@ -3,7 +3,17 @@
 require_once('./includes/class/Database.php');
 
 Database::getInstance(); // test connexion BDD
-?>
-<html>
 
-</html>
+require_once ('./includes/layouts/header.php');
+
+?>
+
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+
+
+
+<?php
+require_once ('./includes/layouts/footer.php');
+?>
