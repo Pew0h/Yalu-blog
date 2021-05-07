6796e617e0996dc7de7fd96ea184da950a3f47ee
Pewoh <burl410@gmail.com>
2021-05-07 16:17:54 +0200

    Add CRUD role

--- includes/class/Role.php
+++ includes/class/Role.php
@@ -8,6 +8,34 @@ class Role
         return $request->fetchAll();
     }
 
+    public static function insertRole(string $nom)
+    {
+        $database = Database::getInstance();
+        $request = $database->prepare('INSERT INTO role(nom) VALUES (:role_nom)');
+        $request->execute(array(
+            'role_nom' => $nom
+        ));
+    }
+
+    function updateRole(int $id, string $nom)
+    {
+        $database = Database::getInstance();
+        $request = $database->prepare('UPDATE role SET nom = :role_nom WHERE id_role = :role_id');
+        $request->execute(array(
+            'role_nom' => $nom,
+            'role_id' => $id
+        ));
+    }
+
+    function deleteRole(int $id)
+    {
+        $database = Database::getInstance();
+        $request = $database->prepare('DELETE FROM role WHERE id_role = :role_id');
+        $request->execute(array(
+            'role_id' => $id
+        ));
+    }
+
 }
 
 ?>
